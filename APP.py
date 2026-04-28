974417bbacbacb024ba14a0cdea9c5c81970b13c
matheuslucca831-a11y <matheuslucca831@gmail.com>
2026-04-28 09:30:33 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -457,6 +457,9 @@ if email_temp:
 # TELA DE LOGIN
 # --------------------------------------------------
 if not st.session_state.get("autenticado", False):
+    # Use isso apenas uma vez para criar o acesso, depois apague o bloco
+if st.button("Executar Criação de Gestor Inicial"):
+    criar_gestor_maximo("Admin", "Admin", "123")
 
     _, col_login, _ = st.columns([1, 1.5, 1])
 
